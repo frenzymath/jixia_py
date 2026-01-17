23c9c1b456f4a735254a20e905d97c471670a640
Yutong Wang <143721150+slashbade@users.noreply.github.com>
2026-01-17 13:47:57 +0800

    style: Add Ruff GitHub Actions workflow (#6)
    * Add Ruff GitHub Actions workflow
    
    * style: ruff check issue
    
    * Add ruff-action step to GitHub workflow
    
    * style: use ruff to format
    
    * fix: rmove uv.lock and add gitignore

--- tests/test_structs.py
+++ tests/test_structs.py
@@ -4,7 +4,9 @@ from jixia.structs import Declaration, InfoTree, Symbol
 
 
 def test_declaration():
-    declarations = Declaration.from_json_file(Path(__file__).parent / "Example.decl.json")
+    declarations = Declaration.from_json_file(
+        Path(__file__).parent / "Example.decl.json"
+    )
     symbols = Symbol.from_json_file(Path(__file__).parent / "Example.sym.json")
     infotree = InfoTree.from_json_file(Path(__file__).parent / "Example.elab.json")
     print(declarations)
